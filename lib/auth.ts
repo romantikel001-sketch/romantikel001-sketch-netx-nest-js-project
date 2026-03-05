8dedb2b2835c6b0e58629bb72b03c89d3e08cedd
Roman <romantikel001@gmail.com>
2026-03-05 14:50:53 +0500

    prisma studio

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import prisma from "./prisma";
+import { twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql"
     }),
+    emailAndPassword: {enabled:true},
+  plugins: [twoFactor()]
 });
