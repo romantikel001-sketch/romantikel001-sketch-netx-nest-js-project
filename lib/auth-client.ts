0c726cabc67e05ea33eb547c069dbe126bcd3e8c
Roman <romantikel001@gmail.com>
2026-03-06 12:51:23 +0500

    fix auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,8 @@
+import { twoFactorClient } from "better-auth/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "http://localhost:3000",
+    plugins: [
+        twoFactorClient() 
+    ]
 })
