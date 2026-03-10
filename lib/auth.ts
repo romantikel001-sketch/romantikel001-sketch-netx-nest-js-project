4d3b0a6165f04ddb536b42f0375136b35ddd9ee0
Roman <romantikel001@gmail.com>
2026-03-10 12:46:47 +0500

    plugin magicclick

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import prisma from "./prisma";
-import { twoFactor } from "better-auth/plugins";
+import { twoFactor, magicLink } from "better-auth/plugins"; 
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql"
     }),
-    emailAndPassword: {enabled:true},
-  plugins: [twoFactor()]
+    emailAndPassword: { 
+        enabled: true,
+        requireEmailVerification: true
+    },
+    plugins: [
+        twoFactor(),
+        magicLink({
+            async sendMagicLink({ url }) {
+                console.log(url);
+            },
+        })
+    ]
 });
